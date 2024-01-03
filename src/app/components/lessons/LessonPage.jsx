ef8f15599d98778c04d5e53928cc817d0cf3ca2d
shiv-anya <sg90883@gmail.com>
2024-01-03 19:45:46 +0530

    added sidebar for lesson page

---
+++ src/app/components/lessons/LessonPage.jsx
@@ -0,0 +1,40 @@
+import { Playfair_Display } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const playfair = Playfair_Display({ subsets: ["latin"], weight: "700" });
+
+export default function LessonPage({ title, src }) {
+  return (
+    <section className="w-[75vw]">
+      <div className="w-[90%] mx-auto">
+        <h2 className={`${playfair.className} text-[3rem] my-5`}>{title}</h2>
+
+        <iframe
+          width="100%"
+          height="350"
+          src="https://www.youtube.com/embed/ScMzIvxBSi4?si=aq3ZN4-42Y4HMuPL"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+        <div className="my-5 flex flex-col gap-5">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum hic
+            voluptates eaque quia, culpa in fugit! Magnam assumenda alias
+            impedit quas doloremque maxime inventore, architecto quisquam quasi
+            ullam minima culpa?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            provident porro non delectus, vero reprehenderit architecto aliquid
+            adipisci recusandae impedit. Labore expedita ducimus qui libero
+            molestiae nemo a architecto nulla!
+          </p>
+        </div>
+        <div className="h-[80vh] mb-5 relative w-full">
+          <Image fill src={src} alt="fruits" className="object-cover" />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/components/lessons/SideBar.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function SideBar({ links, title, type }) {
+  const pathname = usePathname();
+  return (
+    <aside className="w-[25vw] h-[100vh] border-r border-gray-300 sticky top-0 left-0">
+      <nav>
+        <h2 className="text-[1.7rem] px-2 py-5 bg-secondary text-white">
+          {title}
+        </h2>
+        <ul className="h-[70vh] overflow-scroll">
+          {links.map((l) => (
+            <li
+              key={l}
+              className={`${
+                pathname ===
+                `/${type}/lessons/${l
+                  .toLowerCase()
+                  .split(/[^a-zA-Z0-9]+/)
+                  .join("-")}`
+                  ? "text-secondary"
+                  : "text-[#333]"
+              } p-2 border-b border-gray-300 text-lg`}
+            >
+              <Link
+                href={`/${type}/lessons/${l
+                  .toLowerCase()
+                  .split(/[^a-zA-Z0-9]+/)
+                  .join("-")}`}
+              >
+                {l}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
